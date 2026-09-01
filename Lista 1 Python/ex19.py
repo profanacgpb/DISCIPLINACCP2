278a01fe298be5817636d268382564bef8b0aa9f
Zenilo Bismark <zenildobismarkzbrl@gmail.com>
2026-09-01 20:14:10 -0300

    primeiro envio para minha branch

---
+++ Lista 1 Python/ex19.py
@@ -0,0 +1,6 @@
+palavra = input("Digite uma palavra: ").lower
+
+if palavra == python:
+    print("Você digitou Python!")
+else:
+    print("")
---
+++ Lista 1 Python/ex9.py
@@ -0,0 +1,9 @@
+n1 = int(input("Digite o primeiro número: "))
+n2 = int(input("Digite o segundo número"))
+
+if n1 > n2:
+    print(f"O maior número é: {n1}")
+    print(f"O menor número é: {n2}")
+else:
+    print(f"O maior número é: {n2}")
+    print(f"O menor número é: {n1}")
