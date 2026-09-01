8b8c19b74c0600b1a597e52f849b3b07b2bde8b4
dplayerf <danielmotaita@gmail.com>
2026-09-01 20:08:42 -0300

    Adicionando Lista

---
+++ Lista01/exercicio018.py
@@ -0,0 +1,8 @@
+print("-"*12,"Divisivel por 5","-"*12)
+
+num = int(input("Digite um número: "))
+
+if num % 5 == 0:
+    print("Esse número é divisivel por 5.")
+else:
+    print("Esse número não é divisel por 5.")
---
+++ Lista01/exercicio019.py
@@ -0,0 +1,6 @@
+palavra = input("Digite uma palavra: ")
+
+if palavra == "python":
+    print("Você digitou Python.")
+else:
+    print("Você digitou outra palavra.")
