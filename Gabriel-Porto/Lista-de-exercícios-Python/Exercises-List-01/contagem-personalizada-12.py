3f9746833346bb3235c0361a9dcce39c86dfe66b
devgabrielporto07 <estudosgabriel07@gmail.com>
2026-09-01 20:33:20 -0300

    feat: Lista completa 01 do Python

---
+++ Gabriel-Porto/Lista-de-exercícios-Python/Exercises-List-01/contagem-personalizada-12.py
@@ -0,0 +1,4 @@
+number_user = int(input("Digite um número inteiro: ")) # Entrada de dados tipo inteiro
+
+for loop in range (0, number_user, +1): # Estrutura de controle (repetição) (inicio, fim, passo)
+    print (loop) # Exibição
---
+++ Gabriel-Porto/Lista-de-exercícios-Python/Exercises-List-01/media-aluno-07.py
@@ -0,0 +1,6 @@
+media_aluno = float(input("Digite sua média (nota): ")) # Entrada de dados
+
+if media_aluno >= 7: # Condicionais compostas
+    print (f"Aluno aprovado.") # Saída de dados
+else:
+    print (f"Aluno reprovado.") # Saída de dados
