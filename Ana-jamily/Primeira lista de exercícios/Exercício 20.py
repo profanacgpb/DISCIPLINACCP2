c947524042793a747c84894d8b2e7c2ebd06b908
Anajamily19 <jamilebarboza.0007@gmail.com>
2026-09-07 16:56:43 -0300

    Subindo a primeira lista de exercícios

---
+++ Ana-jamily/Primeira lista de exercícios/Exercício 20.py
@@ -0,0 +1,18 @@
+name = str(input("Enter your name: "))
+age = int(input("How old are you:  "))
+note = int(input("Enter your note: "))
+
+print(name)
+print(age)
+
+if age >=18:
+    print("Você é maior de idade")
+
+else:
+    print("Você não é maior de idade")
+
+if note >=7:
+    print("Você foi aprovado!")
+
+else: 
+    print("Você não é maior de idade")
