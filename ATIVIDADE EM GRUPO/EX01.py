a79a12a287f5cce6b7dafc7cee6fc1b3e8da0781
VagnerGG <vagnergomesguimaraes@gmail.com>
2026-09-08 20:23:58 -0300

    Add files via upload
    DANIEL, CAIO E VAGNER

---
+++ ATIVIDADE EM GRUPO/EX01.py
@@ -0,0 +1,17 @@
+alunos = ["Rotherdan", "Gabriel", "Vagner", "Daniel", "Caio"]
+
+print(alunos)
+
+alunos.append("Rafael")
+
+alunos.remove("Gabriel")
+
+print("Primeiros 3:", alunos[:3])
+
+print("Últimos 2:", alunos[-2:])
+
+alunos.reverse()
+print("Invertida:", alunos)
+
+alunos.sort()
+print("Ordenada:", alunos)
