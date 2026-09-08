0a910f6d1e83c90d284e195abe55e6de29454085
devgabrielporto07 <estudosgabriel07@gmail.com>
2026-09-08 19:12:50 -0300

    feat: Desafio 02 Resolvido -Python

---
+++ aula-python-gabriel-porto/aula-02/desafio-02.py
@@ -0,0 +1,22 @@
+# Crie um perfil de aluno com nome, matrícula, curso, idade e média. Exiba os dados de forma organizada.
+
+name = "Gabriel"
+matricula = 4208414
+course = "Ciência da Computação"
+age = 19
+media = 9.0
+matricula_ativa = True
+
+print (name)
+print (matricula)
+print (course)
+print (age)
+print (media)
+print (matricula_ativa)
+
+print (type(name))
+print (type(matricula))
+print (type(course))
+print (type(age))
+print (type(media))
+print (type(matricula_ativa))
