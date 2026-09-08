0d6bde1b89d8535694524136fd3261d217473571
geovaniteixeira750-hub <geovaniteixeira750@gmail.com>
2026-09-08 20:15:20 -0300

    desafios(hilton lucas,gustavo herculano)

---
+++ desafios/desafio2.py
@@ -0,0 +1,19 @@
+notas = [7.5, 8.0, 6.5, 9.0, 5.5]
+
+print("Notas:", notas)
+
+print("Maior nota:", max(notas))
+
+print("Menor nota:", min(notas))
+
+print("Quantidade:", len(notas))
+
+print("Soma:", sum(notas))
+
+media = sum(notas) / len(notas)
+print("Média:", media)
+
+notas.sort()
+print("Notas ordenadas:", notas)
+
+print("3 maiores:", notas[-3:])
