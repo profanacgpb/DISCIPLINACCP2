8a0bc779fc4378556dfb8274fc8d6ec733dfbbbc
crazyfxp <dantasfxp142536@gmail.com>
2026-09-08 19:55:45 -0300

    atividade comentada

--- 20_desafio.py
+++ 20_desafio.py
@@ -1,9 +1,14 @@
+#coleta de informações do aluno
 nome = input("Digite seu nome: ")
 idade = int(input("Digite sua idade: "))
 nota = float(input("Digite sua nota: "))
 
+#nome e idade do aluno
+
 print("Nome:", nome)
 print("Idade:", idade)
+
+#informação sobre a situação do aluno
 
 if idade >= 18:
     print("Situação: maior de idade.")
