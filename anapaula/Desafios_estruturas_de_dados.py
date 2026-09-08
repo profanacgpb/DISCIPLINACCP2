da64d8b2954ebba76a0c7978d040d72326706b3e
Ana Paula Alves <anpaula01001@gmail.com>
2026-09-08 20:02:35 -0300

    Desafio 1 - Estrutura de dados

---
+++ anapaula/Desafios_estruturas_de_dados.py
@@ -0,0 +1,21 @@
+#Desafio 1: Lista de alunos
+
+nome = ["Carlos", "Eduarda", "Ana", "Daniel", "Bruno"]
+print(nome)
+
+nome.append("Fábio")
+nome.remove("Daniel")
+nome.insert(2,"Carla")
+
+print (nome)
+
+print(nome[:3])
+print(nome[3:5])
+
+nome.reverse()
+print(nome)
+
+nome.sort()
+print (nome)
+
+
