36a738e2b86221c809554bdf6062b530a3534cda
Ana Paula Alves <anpaula01001@gmail.com>
2026-09-09 08:17:26 -0300

    Desafio 2 da aula de Estrutura de Dados

--- anapaula/Desafios_Estrutura_de_dados/Desafio_02.py
+++ anapaula/Desafios_Estrutura_de_dados/Desafio_02.py
@@ -1,4 +1,23 @@
 #Desafio 2: Análise de notas
 
-notas [7.5, 8.0, 6.5, 9.0, 5.5]
+notas = [7.5, 8.0, 6.5, 9.0, 5.5]
+print("NOTAS REGISTRADAS:", notas)
+print("\n")
+
+media = sum(notas)/len(notas) 
+
+notas.sort()
+
+print("MENOR NOTA:", notas[0])
+print("MAIOR NOTA:", notas[4])
+print("Quantidade de notas na lista:", len(notas))
+print("Soma das notas:", sum(notas))
+print(f"Média das notas: {media}")
+print("Lista de notas ordenada:", notas)
+
+notas.reverse()
+print("As 3 maiores notas são:", notas[:3])
+
+
+
 
