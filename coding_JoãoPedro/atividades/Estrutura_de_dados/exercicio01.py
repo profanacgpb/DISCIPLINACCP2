4e7c278c39f108b6c8f7a1585b815aa551b3c075
João Pedro Campos dos Santos <joaopedro.campos.cs@gmail.com>
2026-09-10 20:52:16 -0300

    exercicio de estrututa de  dados

---
+++ coding_JoãoPedro/atividades/Estrutura_de_dados/exercicio01.py
@@ -0,0 +1,55 @@
+lista = [0,5,7,3,8,6,2,4,1,9]
+print("\n")
+print(lista)
+
+lista.append(6)
+print("\n")
+print(lista)
+
+lista.insert(3, 7)
+print("\n")
+print(lista)
+
+lista.pop(2)
+print("\n")
+print(lista)
+
+lista.append(4)
+print("\n")
+print(lista)
+
+lista.count(4)
+print("\n")
+print(lista)
+
+lista = [0,5,7,3,8,6,2,4,1,9]
+print("\n")
+print(lista[:3])
+
+lista = [0,5,7,3,8,6,2,4,1,9]
+print("\n")
+print(lista[3:8])
+
+lista = [0,5,7,3,8,6,2,4,1,9]
+print("\n")
+print(lista[::3])
+
+lista = [0,5,7,3,8,6,2,4,1,9]
+print("\n")
+print(lista[-3:])
+
+lista = [0,5,7,3,8,6,2,4,1,9]
+print("\n")
+print(lista[0:6])
+
+lista = [0,5,7,3,8,6,2,4,1,9]
+print("\n")
+print(lista[6])
+
+lista[7] = 12
+print("\n")
+print(lista)
+
+lista.reverse()
+print("\n")
+print(lista)
---
+++ coding_JoãoPedro/atividades/Estrutura_de_dados/tupla.py
@@ -0,0 +1,9 @@
+tupla = (5,3,7,2,9,1,4,8,0,6)
+print(tupla)
+
+#A não é possivel alterar o valor da tupla 
+
+tupla = (5,3,7,2,9,1,4,8,0,6)
+print("\n")
+print(tupla.index(6))
+
