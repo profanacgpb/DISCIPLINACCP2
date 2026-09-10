5ebbf0189dcc3775fe7b3bf24bd14b53a36cfd51
Ana Paula Alves <anpaula01001@gmail.com>
2026-09-10 13:01:10 -0300

    Lista de exercícios 2: Adicionando questões 5, 6 e 7.

--- anapaula/Lista_de_exercícios_02/Questão_02.py
+++ anapaula/Lista_de_exercícios_02/Questão_02.py
@@ -1,11 +1,25 @@
-#QUESTÃO 1: Outras manipulações utilizando a lista da questão 1
+#QUESTÃO 2: Ainda com a lista criada na questão anterior, faça: 
+#a. Retorne os primeiros 3 elementos da lista 
+#b. Retorne os elementos que estão da 3ª posição até a 7ª posição da lista 
+#c. Retorne os elementos da lista de 3 em 3 elementos 
+#d. Retorne os 3 últimos elementos da lista 
+#e. Retorne todos os elementos menos os 4 últimos da lista 
 
 numeros = [0, 2, 4, 1, 5, 9, 8, 6, 3, 7] #Lista da questão 1
 print("Lista:", numeros)
 print("\n")
 
+#Letra a:
 print("Três primeiros elementos:", numeros[:3])
+
+#Letra b:
 print("Elementos da 3ª até a 7ª posição:", numeros[2:7])
+
+#Letra c:
 print("Elementos da lista de 3 em 3:", numeros[0:len(numeros):3])
+
+#Letra d:
 print("Os 3 últimos elementos da lista:", numeros[(len(numeros)-3):len(numeros)])
+
+#Letra e:
 print("Elementos da lista sem os 4 últimos:", numeros[0:len(numeros)-4])
---
+++ anapaula/Lista_de_exercícios_02/Questão_05.py
@@ -0,0 +1,7 @@
+#QUESTÃO 5: Inverta a ordem dos elementos na lista.
+
+numeros = [0, 2, 4, 1, 5, 9, 8, 6, 3, 7] #Lista da questão 1
+print("Lista:", numeros)
+
+numeros.reverse()
+print("Lista invertida:", numeros)
