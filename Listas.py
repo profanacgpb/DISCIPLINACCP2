64d1fdceff756841ff462f7c23837b7db7a34822
Pedro Henrique <pedrixphc23@gmail.com>
2026-09-12 20:14:57 -0300

    Add list operations in Listas.py

---
+++ Listas.py
@@ -0,0 +1,14 @@
+lista= [1, 2, 3, 4, 5, 6, 7, 8, 9]
+print (lista)
+lista.insert(5, 6) 
+print(lista)
+lista.insert(2, 7)
+print(lista)
+lista.remove(7)
+print(lista)
+lista.append(4)
+print(lista)
+lista.count(4)
+ocorrencias= lista.count(4)
+print("O numero 4 aparece", ocorrencias, "vezes")
+
