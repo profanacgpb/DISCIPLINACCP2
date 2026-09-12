d450de46c3886819cf7bbbb3968bef347ff0c26c
Ana Paula Alves <anpaula01001@gmail.com>
2026-09-12 13:18:50 -0300

    Lista de exercícios 2 - Questão 8

--- anapaula/Lista_de_exercícios_02/Questão_03.py
+++ anapaula/Lista_de_exercícios_02/Questão_03.py
@@ -1,4 +1,4 @@
-##QUESTÃO 3: Com a lista das questões anteriores, retorne o 6º elemento da lista. 
+#QUESTÃO 3: Com a lista das questões anteriores, retorne o 6º elemento da lista. 
 
 numeros = [0, 2, 4, 1, 5, 9, 8, 6, 3, 7] #Lista da questão 1
 
---
+++ anapaula/Lista_de_exercícios_02/Questão_08.py
@@ -0,0 +1,53 @@
+#QUESTÃO 8: Crie um dicionário com 5 entradas e suas respectivas chaves e valores. Faça: 
+
+dicionario_animais = {
+    "Animal": "Leão",
+    "Habitat": "Savanas e regiões de pastagem da África",
+    "Alimentacao": "Carne",
+    "Expectativa de vida": "De 10 a 15 anos",
+    "Velocidade aproximada (em km/h)": 80
+}
+
+#Letra a: Imprima todas as chaves do dicionário
+print("CHAVES:", dicionario_animais.keys())
+
+#Letra b: Imprima todos os valores do dicionário
+print("VALORES:", dicionario_animais.values())
+
+#Letra c: Imprima todos os itens do dicionário 
+print("ITENS:", dicionario_animais.items())
+
+#Letra d: Imprima o 2º item do dicionário 
+print("\nSegundo item: ")
+i = 0
+for chave, valor in dicionario_animais.items():
+    i = i + 1
+    if i == 2:
+        print (f"Chave {chave} tem como valor {valor}.")
+
+#Pesquisando, encontrei esta outra forma de fazer a letra d (utilizando list):
+print("SEGUNDO ITEM:", list(dicionario_animais.items())[1])
+print("\n")
+
+#letra e: Imprima o dicionário completo 
+print("DICIONÁRIO COMPLETO: ", dicionario_animais)
+print("\n")
+
+#Letra f: Percorra o dicionário, imprimindo para cada entrada o modelo “(chave) tem como valor (valor)”
+i = 0
+for chave, valor in dicionario_animais.items():
+    print (f"Chave {chave} tem como valor {valor}.")
+    i = i+1
+
+#Letra f: no modelo chave valor
+print("\nITENS NO FORMATO: CHAVE VALOR")
+for chave, valor in dicionario_animais.items():
+    print(chave, valor)
+
+#Letra f utilizando list para mostrar cada item da forma (chave, valor)
+print("\nITENS NO FORMATO: (Chave, Valor):")
+
+for i in range(len(dicionario_animais)):
+    print(f"Item {i+1}:", list(dicionario_animais.items())[i])
+    i = i+1
+
