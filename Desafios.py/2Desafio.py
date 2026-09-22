940db208fb6be3065336d1e93651b9fa5b129ecf
Pedro Henrique <pedrixphc23@gmail.com>
2026-09-22 20:42:17 -0300

    add

--- Desafios.py/2Desafio.py
+++ Desafios.py/2Desafio.py
@@ -1,20 +1,26 @@
 notas= [7.5, 8.0, 6.5, 9.0, 5.5]
 maiornota= max(notas)
 print("Sua maior nota é:", maiornota)
+
 menornota= min(notas)
 print("Sua menor nota é:", menornota)
+
 quantidade= len(notas)
 print("Você ja recebeu:", quantidade, "notas!")
+
 soma= sum(notas)
 print("A soma de todas as notas é:", sum(notas))
+
 media= len(notas)
 print("Sua media é de:", len(notas))
+
 notas_ordenadas=[]
 for i in range(len(notas)):
     menor = min(notas)
     notas_ordenadas.append(menor)
     notas.remove(menor)
 print(notas_ordenadas)
+
 notas = [7.5, 8.0, 6.5, 9.0, 5.5] 
 maiores_valores=[]
 for p in range(3):
